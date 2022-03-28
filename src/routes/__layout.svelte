04334daf924ec330b84aefd02d666e1de6dd0812
Shoubhit Dash <shoubhit2005@gmail.com>
2022-03-28 17:05:07 +0530

    make heading

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,6 @@
   import "../app.css";
 </script>
 
-<slot />
+<div class="container mx-auto my-6 max-w-lg">
+    <slot></slot>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+
+<main>
+    <h1 class="text-2xl text-center font-bold text-gray-100 md:text-3xl">TODOS</h1>
+    
+</main>
