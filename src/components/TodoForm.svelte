61c893869c76503265f568eac42b2cd8b82d49d6
Shoubhit Dash <shoubhit2005@gmail.com>
2022-03-28 17:31:29 +0530

    write todoform markup and style it

---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,11 @@
+<script>
+    
+</script>
+
+<form class="my-6">
+    <div class="flex flex-col text-sm mb-2 ">
+        <label class="font-bold mb-2 text-gray-100" for="todo">TODO</label>
+        <input type="text" name="todo" placeholder="Type todo here" class="appearance-none border-gray-200 py-3 px-4 focus:outline-none rounded-lg font-semibold bg-[#4A5568] text-gray-100 focus:outline-[#319795]">
+    </div>
+    <button type="submit" class="w-full rounded bg-[#319795] hover:bg-[#2C7A7B] transition-colors duration-300 text-gray-100 py-2 px-4 font-semibold">ADD TODO</button>
+</form>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,8 @@
-
-
+<script>
+    import TodoForm from "../components/TodoForm.svelte";
+</script>
 
 <main>
     <h1 class="text-2xl text-center font-bold text-gray-100 md:text-3xl">TODOS</h1>
-    
+    <TodoForm />
 </main>
