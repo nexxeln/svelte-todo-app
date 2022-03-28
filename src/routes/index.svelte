eb6a45f188b7b46618b8b5e68c5f8dfa258588ae
Shoubhit Dash <shoubhit2005@gmail.com>
2022-03-28 17:55:04 +0530

    fix bugs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,13 @@
 <script>
     import TodoForm from "../components/TodoForm.svelte";
+    import Todo from "../components/Todo.svelte";
+    import {todos} from "../stores/todoStore";
 </script>
 
 <main>
     <h1 class="text-2xl text-center font-bold text-gray-100 md:text-3xl">TODOS</h1>
     <TodoForm />
+    {#each $todos as todo}
+        <Todo todo={todo} index={todos.id} />
+    {/each}
 </main>
