3bb93728d2fe17d7b04c0bc9583d1500b35c00bd
Shoubhit Dash <shoubhit2005@gmail.com>
2022-03-28 19:33:18 +0530

    add better stling

---
+++ src/components/Todo.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { deleteTodo, toggleTodo } from "../stores/todoStore";
+
+    export let todo;
+</script>
+
+<li class="flex items-center border-2 border-[#319795] rounded-lg my-2 py-2 px-4">
+    <input type="checkbox" name="completed" checked={todo.completed} on:change={() => toggleTodo(todo.id)} class="mr-2 form-checkbox h-6 w-6 appearance-none border-2 focus:outline-none rounded-md border-[#319795] checked:border-[#81E6D9] checked:bg-[#81E6D9]">
+    <span class={`flex-1 text-gray-100 text-lg font-base ${todo.completed ? "line-through" : ""}`}>{todo.text}</span>
+    <button type="button" class="text-sm font-semibold bg-red-500 hover:bg-red-600 transition-colors duration-300 text-gray-100 py-1 px-2 rounded focus:outline-none" on:click={() => deleteTodo(todo.id)}>DELETE</button>
+</li>
--- src/components/TodoForm.svelte
+++ src/components/TodoForm.svelte
@@ -13,8 +13,8 @@
 
 <form class="my-6" on:submit|preventDefault={handleSubmit}>
     <div class="flex flex-col text-sm mb-2 ">
-        <label class="font-bold mb-2 text-gray-100" for="todo">TODO</label>
+        <label class="font-bold mb-2 text-gray-100" for="todo">ADD TODO</label>
         <input type="text" name="todo" bind:value={todo} placeholder="Type todo here" class="appearance-none border-gray-200 py-3 px-4 focus:outline-none rounded-lg font-semibold bg-[#4A5568] text-gray-100 focus:outline-[#319795]">
     </div>
-    <button type="submit" class="w-full rounded bg-[#319795] hover:bg-[#2C7A7B] transition-colors duration-300 text-gray-100 py-2 px-4 font-semibold">ADD TODO</button>
+    <button type="submit" class="w-full rounded bg-[#319795] hover:bg-[#2C7A7B] transition-colors duration-300 text-gray-100 py-2 px-4 font-semibold">ADD</button>
 </form>
